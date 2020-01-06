732db6f697e54545fd47f9c65e12d27755894d12
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-01-06 09:41:48 +0000

    Add custom hooks: withAuthChecker, axiosWithAuth; Add Register form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import './App.css';
-import { Route } from 'react-router-dom'
-import Navbar from './component/navbar';
-import Footer from './component/footer';
-import Login from './component/login';
-import Home from './component/home';
-import Dashboard from './component/dashboard';
-import Register from './component/register';
+import React, { useState } from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Navbar from "./component/navbar";
+import Footer from "./component/footer";
+import Login from "./component/login";
+import Home from "./component/home";
+import Dashboard from "./component/dashboard";
+import Register from "./component/register";
+import withAuthChecker from "./custom-hooks/withAuthChecker";
 
 function App() {
+  const [registerForm, setRegisterForm] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: ""
+  });
+
   return (
     <div className="App">
       <Navbar />
-      <Route exact path='/'component={Home} />
-      <Route exact path='/login'component={Login} />
-      <Route exact path='/dashboard'component={Dashboard} />
-      <Route exact path='/register'component={Register} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Route
+        exact
+        path="/register"
+        render={() => (
+          <Register
+            setRegisterForm={setRegisterForm}
+            registerForm={registerForm}
+          />
+        )}
+      />
       <Footer />
     </div>
   );
