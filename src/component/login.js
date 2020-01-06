158faed92c7751d99837e26d06972ff79ac3ea6e
niklasbec <niklasbeckerr@gmail.com>
2020-01-06 10:36:23 +0100

    header, footer and login form done, login still needs axios

--- src/component/login.js
+++ src/component/login.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 
-function Login() {
+function Login(props) {
+
+    const handleSubmit = event => {
+        event.preventDefault()
+    }
+
+        const handleChange = event => {
+            props.setLoginFormValues({...props.loginFormValues,
+                [event.target.name]: event.target.value
+            })
+            console.log(props.state)
+    }
+
   return (
     <div>
-      
+      <form className='login-form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} name='email' type='text' value={props.loginFormValues.email} placeholder='Email' />
+          <input onChange={handleChange} name='password' type='password' value={props.loginFormValues.password} placeholder='Password' />
+          <input type='submit' id='submit'/>
+      </form>
     </div>
   );
 }
