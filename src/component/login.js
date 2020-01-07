6bb715dd8f1c58c65a6c2e0e87040d730fa99291
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-01-07 13:45:41 +0000

    Clean code

--- src/component/login.js
+++ src/component/login.js
@@ -1,33 +1,7 @@
 import React from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
 
 function Login(props) {
-  // const handleSubmit = e => {
-  //   e.preventDefault();
-
-  //   axios
-  //     .post("https://split-the-bill-api.herokuapp.com/api/auth/login", {
-  //       email: props.loginFormValues.email,
-  //       password: props.loginFormValues.password
-  //     })
-  //     .then(response => {
-  //       localStorage.setItem("token", response.data.token);
-  //       props.history.push("/dashboard");
-
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // };
-
-  // const handleChange = event => {
-  //   props.setLoginFormValues({
-  //     ...props.loginFormValues,
-  //     [event.target.name]: event.target.value
-  //   });
-  // };
-
   return (
     <div>
       <form className="forms" onSubmit={props.handleSubmit}>
