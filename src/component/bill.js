12582a90a4592ae9f5938841e3cea7b43b603f0b
niklasbec <niklasbeckerr@gmail.com>
2020-01-08 14:22:50 +0100

    bill component

--- src/component/bill.js
+++ src/component/bill.js
@@ -14,7 +14,12 @@ function Bill(props) {
   return (
     <div className="bill">
       <h1>{thisBill[0].title}</h1>
-      <p>HIIIII</p>
+      <p><span className='underline'>Total:</span> <br /><br /> {thisBill[0].amount}</p>
+      <p><span className='underline'>Per person:</span><br /><br /> {thisBill[0].amount / 1 + thisBill[0].splits.length}</p>
+      <p><span className='underline'>Status:</span> <br /><br />{thisBill[0].status}</p>
+      <p><span className='underline'>Created:</span> <br /><br /> {thisBill[0].createdAt}</p>
+      <p><span className='underline'>Updated:</span> <br /><br /> {thisBill[0].updatedAt}</p>
+      <button>Edit</button>
     </div>
   );
 }
