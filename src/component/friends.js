4215cef691f9d27a29a525b5dbd3ff0756e910c2
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-01-10 10:02:27 +0000

    Add Friends page

--- src/component/friends.js
+++ src/component/friends.js
@@ -1,17 +1,19 @@
 import React from "react";
+import DenseTable from "./Table";
 
 function Friends(props) {
   return (
     <div>
-      <h1>FRIENDS</h1>
-      <div className='pointer' onClick={props.onClick}>
+      <h2>FRIENDS</h2>
+      <div className="pointer" onClick={props.onClick}>
         <img
           className="slide-arrow"
           src="https://i.imgur.com/spe9HXm.png"
           width="100px"
-          alt='animation logo'
+          alt="animation logo"
         />
       </div>
+      <DenseTable allUsers={props.allUsers} />
     </div>
   );
 }
